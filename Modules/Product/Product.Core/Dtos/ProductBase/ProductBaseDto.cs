5dc3c635e9b5934931f3289bc124489dccc1fa1e
Maks Michalski <mamich1998@gmail.com>
2024-06-25 22:04:12 +0200

    Dodanie Id do ProductBaseDto

--- Modules/Product/Product.Core/Dtos/ProductBase/ProductBaseDto.cs
+++ Modules/Product/Product.Core/Dtos/ProductBase/ProductBaseDto.cs
@@ -7,12 +7,15 @@ public class ProductBaseDto
     public ProductBaseDto(ProductBaseEntity entity)
     {
         CategoryName = entity.Category.Name;
+        Id = entity.Id;
         Name = entity.Name;
         ProductCount = entity.Products.Count();
         ProductParameterCount = entity.ProductParameters.Count();
     }
 
     public string CategoryName { get; }
+
+    public Guid Id { get; set; }
 
     public string Name { get; }
 
