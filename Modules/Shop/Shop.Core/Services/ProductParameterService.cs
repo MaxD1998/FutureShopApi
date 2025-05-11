3e55ee13ed6ce1d10c641b65293603d5529816dd
Maks Michalski <mamich1998@gmail.com>
2025-05-11 07:45:16 +0200

    Refaktoryzacja projektu

---
+++ Modules/Shop/Shop.Core/Services/ProductParameterService.cs
@@ -0,0 +1,23 @@
+﻿using Shared.Core.Bases;
+using Shared.Core.Dtos;
+using Shop.Core.Dtos.ProductParameter;
+using Shop.Infrastructure.Repositories;
+
+namespace Shop.Core.Services;
+
+public interface IProductParameterService
+{
+    Task<ResultDto<List<ProductParameterFlatDto>>> GetListByProductIdAsync(Guid productId, CancellationToken cancellationToken);
+}
+
+public class ProductParameterService(IProductParameterRepository productParameterRepository) : BaseService, IProductParameterService
+{
+    private readonly IProductParameterRepository _productParameterRepository = productParameterRepository;
+
+    public async Task<ResultDto<List<ProductParameterFlatDto>>> GetListByProductIdAsync(Guid productId, CancellationToken cancellationToken)
+    {
+        var results = await _productParameterRepository.GetListByProductIdAsync(productId, ProductParameterFlatDto.Map(productId), cancellationToken);
+
+        return Success(results);
+    }
+}
